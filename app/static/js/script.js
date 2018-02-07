50b07ed0a728662d1db0db5167df504930e68bb1
Fenna <fennadewilde@hotmail.com>
2018-02-07 13:38:17 +0100

    setup finished

---
+++ app/static/js/script.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+    let settings = {
+        sections: document.querySelectorAll('section')
+    }
+
+    const app = {
+        init() {
+           sections.toggle()
+            routes.init()
+        }
+    }
+
+    const routes = {
+        init() {
+            window.addEventListener("hashchange", function () {
+                sections.toggle()
+            });
+        }
+    }
+
+    const sections = {
+        toggle() {
+            let newHash = window.location.hash
+            let newRoute = newHash.split('#')
+
+            settings.sections.forEach(function (el) {
+                if (el.id === newRoute[1]) {
+                    el.classList.remove('hidden')
+                } else {
+                    el.classList.add('hidden')
+                }
+            })
+        }
+    }
+
+    app.init()
+})()
