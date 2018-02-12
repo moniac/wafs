9ab610825365b03f49d0c55d78d0d531be4febec
Fenna <fennadewilde@hotmail.com>
2018-02-12 13:00:34 +0100

    router

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,34 +2,9 @@
     'use strict';
     const app = {
         init() {
-            sections.toggle()
-            routes.init()
-        }
-    }
-
-    const routes = {
-        init() {
             API.getData()
-            window.addEventListener("hashchange", function (event) {
-                sections.toggle()
-
-            })
-        }
-    }
-
-    const sections = {
-        selector: document.querySelectorAll('section'),
-        toggle() {
-            let newHash = window.location.hash
-            let newRoute = newHash.split('#')
-
-            this.selector.forEach(function (el) {
-                if (el.id === newRoute[1]) {
-                    el.classList.remove('hidden')
-                } else {
-                    el.classList.add('hidden')
-                }
-            })
+            sections.toggle('home')
+            routes.init()
         }
     }
 
@@ -37,22 +12,48 @@
         getData() {
             let request = new XMLHttpRequest();
             request.open('GET', 'http://behance.net/v2/users/rooaahsab16?api_key=rWYfki0K9PioDYZZcrvKGa64xBzcAeaX', true);
-            request.onload = function() {
+            request.onload = function () {
                 if (request.status >= 200 && request.status < 400) {
                     // Success!
-                    let data = JSON.parse(request.responseText);
-                    console.log(data)
+                    let data = JSON.parse(request.responseText)
                 } else {
                     // We reached our target server, but it returned an error
-
                 }
-            };
+            }
 
-            request.onerror = function() {
+            request.onerror = function () {
                 // There was a connection error of some sort
-            };
+            }
 
             request.send();
+        }
+    }
+
+    const routes = {
+        init() {
+            routie({
+                '': function () {
+                    sections.toggle('home')
+                },
+                'about-me': function () {
+                    sections.toggle('about-me')
+                },
+                'work': function () {
+                    sections.toggle('work')
+                }
+            })
+        }
+    }
+
+    const sections = {
+        selector: document.querySelectorAll('section'),
+        toggle(hash) {
+            this.selector.forEach(function (el) {
+                el.classList.add('hidden')
+                if (el.id === hash) {
+                    el.classList.remove('hidden')
+                }
+            })
         }
     }
 
