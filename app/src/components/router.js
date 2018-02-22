554ada3b0264b4a1bfc45ae564fcdcc84a09459b
Mohammed Mulazada <thisismoniac@gmail.com>
2018-02-22 20:46:24 +0100

    split path logic into seperate object

--- app/src/components/router.js
+++ app/src/components/router.js
@@ -3,6 +3,7 @@ import transparency from './transparency'
 import sections from './sections'
 import helpers from './helpers'
 import api from './api'
+import paths from './paths'
 
 const router = {
 	init() {
@@ -11,71 +12,10 @@ const router = {
 				sections.toggle("home")
 			},
 			'pokemon': function() {
-				sections.toggle("pokemon")
-				helpers.toggleLoader()
-				api.call('https://pokeapi.co/api/v2/pokemon/?limit=151')
-					.then((data) => {
-						helpers.toggleLoader()
-						const directives = {
-							name: {
-								href() {
-									return `#pokemon/${this.name}`
-								}
-							}
-						}
-
-						let dataPokemon = data.results.map(function(i) {
-							return {
-								name: i.name,
-								url: i.url,
-								count: (0+i)
-							}
-						}).sort((a, b) => a.name.localeCompare(b.name))
-
-						const pokemonInput = document.querySelector('#pokemon > input')
-
-						pokemonInput.addEventListener('keyup', (e) => {
-							const filteredPokemon = dataPokemon.filter((x, i) => {
-								return x.name.startsWith(e.target.value)
-							})
-
-							Transparency.render(document.querySelector('#pokemon-list'), filteredPokemon, directives)
-							document.querySelector('#pokemon > p').innerHTML = filteredPokemon.length
-						})
-
-						Transparency.render(document.querySelector('#pokemon-list'), dataPokemon, directives)
-						document.querySelector('#pokemon > p').innerHTML = dataPokemon.length
-					})
-					.catch(reason => console.log(reason.message))
+				paths.pokemonList()
 			},
 			'pokemon/?:name': function(name) {
-				helpers.toggleLoader()
-				api.call(`https://pokeapi.co/api/v2/pokemon/${name}`).then((data) => {
-					helpers.toggleLoader()
-					// extract the pokemon data
-					const pokeDetails = {
-						name: data.name,
-						id: data.id,
-						sprite: data.sprites.front_default
-					}
-					// additional data for the html element
-					const directives = {
-						sprite: {
-							src() {
-								return `${this.sprite}`
-							},
-							alt() {
-								// still providing a dynamic alt
-								return `A sprite image of the pokemon ${this.name}`
-							}
-						}
-					}
-
-					sections.toggle(name)
-					document.querySelector('#pokemon-detail').classList.remove('hidden')
-					document.querySelector('#pokemon > input').value = ''
-					Transparency.render(document.querySelector('#pokemon-detail'), pokeDetails, directives)
-				})
+				paths.pokemonDetail(name)
 			}
 		})
 	},
