57b1fd2413ad8d5a402962d7f1823f5e46c78841
Mohammed Mulazada <thisismoniac@gmail.com>
2018-02-23 10:23:45 +0100

    Added basic error message

--- app/src/components/helpers.js
+++ app/src/components/helpers.js
@@ -1,9 +1,14 @@
+import sections from './sections'
+
 const helpers = {
 	splitHash(hash) {
 		return hash.split('#')[1]
 	},
 	toggleLoader() {
 		document.querySelector('.loader').classList.toggle('hidden')
+	},
+	pageError() {
+		sections.init()
 	}
 }
 
